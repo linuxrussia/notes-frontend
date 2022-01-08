2e0608d2aa85cd0bf9551ca03d9c61aa7a8644a2
Edward Kobylyanets <kobylyanets@gmail.com>
2022-01-08 04:19:42 +0300

    add prettier and eslint configs

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Index = ({ global: { test } }) => {
-    return <div>Проверка: {test} : {test}</div>
+  return (
+    <div>
+      Проверка: {test} : {test}
+    </div>
+  );
 };
 
 export const getStaticProps = async () => {
-    return {
-        props: {
-            global: { test: 1 }
-        }
+  return {
+    props: {
+      global: { test: 1 }
     }
+  };
 };
 
 export default Index;
